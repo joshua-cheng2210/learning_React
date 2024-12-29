64bbbd8ab8d4cbc248dcd6b64ec249609c871d7f
chen7647 <chen7647@umn.edu>
2024-12-29 16:51:29 -0600

    initializing everything for now

---
+++ src/Components/Greet.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// function Greet() {
+//     return <h1>Hello Joshua Cheng</h1>
+// }
+
+const Greet = () => <h1> Hello joshua cheng</h1>
+export default Greet;
+
+// this program has a functional components designed to display a simple message
+// key important features:
+// - absence of the "this" keyword
+// - a solution without using state
+// - normally used for returning UI
+// - very hard to debug since there is no "this"
---
+++ src/Components/Welcome.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class Welcome extends Component {
+    render() {return <h1>Class Component</h1>;}
+}
+
+export default Welcome;
+
+// this is class component designed to display a simple message
+// key important featurres:
+// - more feature rich
+// - provide lifecycle hooks
+// - complex UI logic
